340237fb94d7b01eaa41e0c6983062e9d73db18b
清水理 <shimizuosamu@shimizuosamunoMacBook-Air.local>
2026-05-10 08:54:20 +0900

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiniVulnerableLab.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
